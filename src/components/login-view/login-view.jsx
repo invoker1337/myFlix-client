63391141bc47e12bdc193b9e9ae152f9d7096fcf
invoker1337 <91456039+invoker1337@users.noreply.github.com>
2022-01-18 12:54:53 +0100

    added login-view

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import './login-view.scss';
 
 export function LoginView(props) {
     const [username, setUsername] = useState('');
@@ -26,3 +28,10 @@ export function LoginView(props) {
         </form>
     );
 }
+LoginView.propTypes = {
+    user: PropTypes.shape({
+        username: PropTypes.string.isRequired,
+        password: PropTypes.string.isRequired
+    }),
+    onLoggedIn: PropTypes.func.isRequired,
+};
